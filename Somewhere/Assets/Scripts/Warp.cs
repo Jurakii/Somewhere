d165867cc713c00f409afb5d7f3414edb3d0426c
Jurakii <ncrouse518@gmail.com>
2022-04-18 19:43:21 -0700

    Warp

---
+++ Somewhere/Assets/Scripts/Warp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour
+{
+    public GameObject Entrance;
+    public Transform Exit;
+    public Transform Player;
+    public Transform Camera;
+
+    private void onTriggerEnter2D()
+    {
+            Debug.Log("In");
+            /*if (Input.GetKeyDown(KeyCode.E))
+            {
+                Player.position = Exit.position;
+            }*/
+    }
+
+}
