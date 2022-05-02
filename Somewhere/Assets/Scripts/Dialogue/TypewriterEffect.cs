f68f5c4f1eb519b3217656a7707a8fd99f89e122
Jurakii <ncrouse518@gmail.com>
2022-05-01 21:35:34 -0700

    Dialogue System

---
+++ Somewhere/Assets/Scripts/Dialogue/TypewriterEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TypewriterEffect : MonoBehaviour
+{
+    [SerializeField] private float typewriterSpeed = 50f;
+
+    public Coroutine Run(string textToType, TMP_Text textLabel)
+    {
+        return StartCoroutine(routine: TypeText(textToType, textLabel));
+    }
+
+    private IEnumerator TypeText(string textToType, TMP_Text textLabel)
+    {
+        textLabel.text = string.Empty;
+
+        float t = 0;
+        int charindex = 0;
+
+        while (charindex < textToType.Length)
+        {
+            t += Time.deltaTime * typewriterSpeed;
+            charindex = Mathf.FloorToInt(t);
+            charindex = Mathf.Clamp(value: charindex, min: 0, max: textToType.Length);
+
+            textLabel.text = textToType.Substring(startIndex:0, length:charindex);
+
+            yield return null;
+
+            textLabel.text = textToType;
+        }
+    }
+}
