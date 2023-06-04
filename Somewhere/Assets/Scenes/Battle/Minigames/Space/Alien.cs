081c5fbe0793deb23dc9ed69dd4f8b9a2ffbcede
Jurakii <102999225+Jurakii@users.noreply.github.com>
2023-06-04 00:39:41 -0700

    .

---
+++ Somewhere/Assets/Scenes/Battle/Minigames/Space/Alien.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alien : MonoBehaviour
+{
+    public float moveSpeed = 5f;          // Speed of object movement
+    public float maxXPosition = 10f;      // Maximum x position before flipping direction
+    private bool moveRight = true;        // Flag to track the direction of movement
+
+    private void Update()
+    {
+        // Calculate the direction of movement
+        float direction = moveRight ? 1f : -1f;
+
+        // Move the object horizontally
+        transform.Translate(Vector3.right * direction * moveSpeed * Time.deltaTime);
+
+        // Check if the object has reached the maximum x position
+        if (moveRight && transform.position.x >= maxXPosition)
+        {
+            // Change direction to move left
+            moveRight = false;
+            transform.Translate(Vector3.down * 1f);
+        }
+        else if (!moveRight && transform.position.x <= -maxXPosition)
+        {
+            // Change direction to move right
+            moveRight = true;
+            transform.Translate(Vector3.down * 1f);
+        }
+    }
+}
---
+++ Somewhere/Assets/Scenes/Battle/Minigames/Space/SpaceGame.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpaceGame : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform enemyContainer;
+    public Transform player;
+    public Transform bulletPrefab;
+    public Transform bulletContainer;
+    public Text scoreText;
+    public Text gameOverText;
+
+
+    public float playerSpeed = 1f;
+    public float shootSpeed = 1f;
+
+    private int score;
+    private bool isGameOver;
+
+    private void Start()
+    {
+        score = 0;
+        isGameOver = false;
+        gameOverText.gameObject.SetActive(false);
+
+        // Spawn enemies
+        SpawnEnemies();
+    }
+
+    private void Update()
+    {
+        if (isGameOver)
+            return;
+
+        // Move the player
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        player.Translate(Vector3.right * moveHorizontal * Time.deltaTime * playerSpeed);
+
+        // Shoot bullets
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShootBullet();
+        }
+
+        // Check if all enemies are destroyed
+        if (enemyContainer.childCount == 0)
+        {
+            isGameOver = true;
+            gameOverText.gameObject.SetActive(true);
+        }
+    }
+
+private void SpawnEnemies()
+{
+    float initialX = -2.5f;
+    float initialY = 2f;
+    float spacingX = 1.5f;
+    float spacingY = 1.2f;
+
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 6; col++)
+        {
+            Vector3 position = enemyContainer.position + new Vector3(initialX + spacingX * col, initialY - spacingY * row, 0f);
+            Instantiate(enemyPrefab, position, Quaternion.identity, enemyContainer);
+        }
+    }
+}
+
+
+    private void ShootBullet()
+    {
+        Vector3 bulletPosition = player.position + Vector3.up;
+        Transform bullet = Instantiate(bulletPrefab, bulletPosition, Quaternion.identity, bulletContainer);
+        bullet.GetComponent<Rigidbody2D>().velocity = Vector2.up * shootSpeed;
+    }
+
+    public void IncreaseScore(int points)
+    {
+        score += points;
+        scoreText.text = "" + score;
+    }
+}
