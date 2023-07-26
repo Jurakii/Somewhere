91e5e21bb96ddae59fc7eb1cfa100e027fd31bc6
Jurakii <102999225+Jurakii@users.noreply.github.com>
2023-07-25 17:43:55 -0700

    .

--- Somewhere/Assets/Scripts/FollowScript.cs
+++ Somewhere/Assets/Scripts/FollowScript.cs
@@ -6,6 +6,7 @@ public class FollowScript : MonoBehaviour
     public Vector3 cameraOffset;
     public float followSpeed = 10f;
     public float xMin = 0f;
+    public float maxX = float.MaxValue; // Add the maxX variable here
     Vector3 velocity = Vector3.zero;
     public bool isLocked = false;
 
@@ -13,11 +14,11 @@ public class FollowScript : MonoBehaviour
     {
         if (!isLocked)
         {
-        Vector3 targetPos = target.position + cameraOffset;
-        Vector3 clampedPos = new Vector3(Mathf.Clamp(targetPos.x, xMin, float.MaxValue), targetPos.y, targetPos.z);
-        Vector3 smoothPos = Vector3.SmoothDamp(transform.position, clampedPos, ref velocity, followSpeed * Time.fixedDeltaTime);
+            Vector3 targetPos = target.position + cameraOffset;
+            Vector3 clampedPos = new Vector3(Mathf.Clamp(targetPos.x, xMin, maxX), targetPos.y, targetPos.z); // Use maxX for clamping
+            Vector3 smoothPos = Vector3.SmoothDamp(transform.position, clampedPos, ref velocity, followSpeed * Time.fixedDeltaTime);
 
-        transform.position = smoothPos;
+            transform.position = smoothPos;
         }
     }
 }
