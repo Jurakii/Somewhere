d7183fe07b40a78ebe686f0a86c48aff84d9ce24
Jurakii <102999225+Jurakii@users.noreply.github.com>
2023-07-25 23:52:36 -0700

    .

--- Somewhere/Assets/Scenes/Battle/BattleManager.cs
+++ Somewhere/Assets/Scenes/Battle/BattleManager.cs
@@ -13,48 +13,47 @@ public class BattleManager : MonoBehaviour
     private bool isBattleActive; // Flag to determine if the battle is active
     private List<string> shuffledScenes; // Shuffled list of scene names
     private int currentSceneIndex; // Index of the current scene in the shuffled array
-    private float sceneTransitionTimer; // Timer for the scene transition buffer
+    private bool isLoadingScene; // Flag to check if a scene is currently being loaded
     private List<Scene> loadedScenes; // List of loaded scenes
 
     private void Start()
     {
         StartBattle(); // Start the battle when the script is enabled
     }
-
-    private void Update()
+private void Update()
+{
+    if (isBattleActive)
     {
-        if (isBattleActive)
+        // Decrement the timer each frame based on the time delta
+        battleTimer -= Time.deltaTime;
+
+        if (battleTimer <= 0f)
         {
-            // Decrement the timer each frame based on the time delta
-            battleTimer -= Time.deltaTime;
+            // Timer reached zero, perform actions for both player and enemy attacks
+            PerformPlayerAttack();
+            PerformEnemyAttack();
 
-            if (battleTimer <= 0f)
+            if (PlayerOrEnemyHealthIsZero())
             {
-                // Timer reached zero, perform actions for both player and enemy attacks
-                PerformPlayerAttack();
-                PerformEnemyAttack();
-
-                if (PlayerOrEnemyHealthIsZero())
+                StopBattle(); // Call StopBattle method here
+            }
+            else
+            {
+                if (!isLoadingScene)
                 {
-                    StopBattle();
-                }
-                else
-                {
-                    sceneTransitionTimer -= Time.deltaTime;
-                    if (sceneTransitionTimer <= 0f)
-                    {
-                        LoadNextSceneAdditively();
-                    }
+                    StartCoroutine(LoadNextSceneAfterDelay());
                 }
             }
         }
     }
+}
+
 
     private void StartBattle()
     {
         battleTimer = battleDuration; // Reset the battle timer
         isBattleActive = true; // Set the battle as active
-        sceneTransitionTimer = 0f; // Reset the scene transition timer
+        isLoadingScene = false; // Reset the scene loading flag
         loadedScenes = new List<Scene>(); // Initialize the list of loaded scenes
 
         // Shuffle the scene array and create a shuffled list
@@ -62,7 +61,24 @@ public class BattleManager : MonoBehaviour
         ShuffleList(shuffledScenes);
         currentSceneIndex = 0;
 
+        // Unload any previously loaded scenes
+        UnloadAllLoadedScenes();
+
         LoadCurrentSceneAdditively();
+    }
+private void StopBattle()
+{
+    isBattleActive = false; // Set the battle as inactive
+    // Perform any additional actions to clean up the battle, reset variables, etc.
+}
+    private IEnumerator LoadNextSceneAfterDelay()
+    {
+        isLoadingScene = true;
+
+        yield return new WaitForSeconds(sceneTransitionDelay);
+
+        LoadNextSceneAdditively();
+        isLoadingScene = false;
     }
 
     private void LoadCurrentSceneAdditively()
@@ -71,25 +87,44 @@ public class BattleManager : MonoBehaviour
         SceneManager.LoadScene(currentSceneName, LoadSceneMode.Additive);
         Scene loadedScene = SceneManager.GetSceneByName(currentSceneName);
         loadedScenes.Add(loadedScene);
-        sceneTransitionTimer = sceneTransitionDelay;
     }
 
     private void LoadNextSceneAdditively()
     {
+        UnloadCurrentSceneAdditively(); // Unload the current scene first
+
         currentSceneIndex++;
         if (currentSceneIndex >= shuffledScenes.Count)
         {
             currentSceneIndex = 0; // Start from the beginning if all scenes have been played
+            ShuffleList(shuffledScenes); // Shuffle the scene list again for the next cycle
         }
-        UnloadCurrentSceneAdditively();
         LoadCurrentSceneAdditively();
     }
 
     private void UnloadCurrentSceneAdditively()
     {
-        Scene currentScene = loadedScenes[currentSceneIndex];
-        SceneManager.UnloadSceneAsync(currentScene);
-        loadedScenes.RemoveAt(currentSceneIndex);
+        if (currentSceneIndex >= 0 && currentSceneIndex < loadedScenes.Count)
+        {
+            Scene currentScene = loadedScenes[currentSceneIndex];
+            if (currentScene.isLoaded)
+            {
+                SceneManager.UnloadSceneAsync(currentScene);
+                loadedScenes.RemoveAt(currentSceneIndex);
+            }
+        }
+    }
+
+    private void UnloadAllLoadedScenes()
+    {
+        foreach (var scene in loadedScenes)
+        {
+            if (scene.isLoaded)
+            {
+                SceneManager.UnloadSceneAsync(scene);
+            }
+        }
+        loadedScenes.Clear();
     }
 
     private void PerformPlayerAttack()
@@ -108,11 +143,6 @@ public class BattleManager : MonoBehaviour
     {
         // Implement your logic here to check if the player's health or the enemy's health reaches zero
         return false;
-    }
-
-    private void StopBattle()
-    {
-        isBattleActive = false; // Set the battle as inactive
     }
 
     // Utility function to shuffle a list using the Fisher-Yates algorithm
